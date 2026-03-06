b8feaed9468bb6dacf40e21c76cb16879dfc6e8a
Eliot Briefer <briefereliot@gmail.com>
2026-03-05 17:27:50 -0800

    Added 2025 boat path

--- ships_log.js
+++ ships_log.js
@@ -1,3 +1,21 @@
+async function loadArrayFromJsonFile(filePath) {
+  try {
+    const response = await fetch(filePath);
+    // The .json() method automatically parses the JSON content into a JS object/array
+    const data = await response.json(); 
+    
+    if (Array.isArray(data)) {
+      return data; // Returns the array
+    } else {
+      console.error("JSON file did not contain a root-level array.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching or parsing JSON:", error);
+    return null;
+  }
+}
+
 const testMap = L.map('map').setView([48.5023, -122.8463], 9);
 const tiles = L.tileLayer('./tiles/{z}/{x}/{y}.png', {
     maxZoom: 12,
@@ -6,18 +24,13 @@ const tiles = L.tileLayer('./tiles/{z}/{x}/{y}.png', {
 
 testMap.doubleClickZoom.disable();
 
-const circle = L.circleMarker([48.5023, -122.8463], {
-    color: 'red',
-    fillOpacity: 0,
-    radius: 4,
-    weight: 3
-}).addTo(testMap);
-
 boatPath = L.polyline([], {
     color: 'red',
-    weight: 1,
+    weight: 2,
     dashArray: '4 4'
 }).addTo(testMap);
+
+markerPoints = [];
 
 function onMapClick(e) {
     /*L.circleMarker(e.latlng, {
@@ -36,9 +49,34 @@ function onMapDblClick(e) {
         radius: 4,
         weight: 3
     }).addTo(testMap);
+    markerPoints.push(e.latlng)
+    console.log(markerPoints)
     //remove last double click line
     boatPath.setLatLngs(boatPath.getLatLngs().slice(0, -2));
+    console.log(boatPath.getLatLngs());
 }
 
 testMap.on('click', onMapClick);
 testMap.on('dblclick', onMapDblClick)
+
+async function loadBoatPath() {
+    const pathArray = await loadArrayFromJsonFile('2025_path.json');
+    const pinsArray = await loadArrayFromJsonFile('2025_pins.json');
+
+    const oldBoatPath = L.polyline(pathArray, {
+        color: 'red',
+        weight: 2,
+        dashArray: '4 4'
+    }).addTo(testMap);
+
+    for(var i = 0; i < pinsArray.length; i++){
+        L.circleMarker(pinsArray[i], {
+            color: 'red',
+            fillOpacity: 0,
+            radius: 4,
+            weight: 3
+        }).addTo(testMap);
+    }
+}
+
+loadBoatPath();
